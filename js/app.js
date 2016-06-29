99815382ae9bb33194ff796268e2f89548840006
superwoods <st_sister@icloud.com>
2016-06-29 21:43:21 +0800

    完成“Buy Shoes 网页布局”

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
 window.onload = function() {
-  console.log("page loaded");
+    console.log("page loaded");
+    makeCartScrollNicely();
+    makeSiteScrollNicely()
+}
+
+function makeCartScrollNicely() {
+    var cart = document.querySelector('.cart__content');
+    // console.log(cart);
+    Ps.initialize(cart);
+}
+
+function makeSiteScrollNicely() {
+    var site = document.querySelector('.site');
+    // console.log(cart);
+    Ps.initialize(site);
 }
